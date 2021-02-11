af3ea85469a3b78c7e54df7fda7586d7c99dd0e9
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-02-10 16:34:48 -0800

    create reusable Dropdown componenet for horns and keyword filter

--- src/App.js
+++ src/App.js
@@ -3,22 +3,47 @@ import './App.css';
 import Header from './My Components/header.js';
 import ImageList from './My Components/ImageList.js';
 import images from './data.js';
+import Dropdown from './My Components/Dropdown';
 
 
 export default class App extends React.Component {
   // initalize state //
   state = {
     keyword: '',
+    // initalize state for horns //
+    horns: '',
   }
+
+  // creating handlers to set horns and keyword 
+
+  keywordChange = (e) => {
+    this.setState({
+      keyword: e.target.value
+    })
+  }
+
+  hornsChange = (e) => {
+    this.setState({
+      horns: Number(e.target.value)
+    })
+  }
+
+  //
   
   render () {
     const filteredAnimals = images.filter((ImageList) => {
-      // if the user does not select a specific keyword, show all //
+      // if the user does not select a specific keyword or horns, show all //
       
-       if (!this.state.keyword) return true;
-      // if the user selects a specific keyword, show image with the same keyword //
+       if (!this.state.keyword && !this.state.horns) return true;
       
+       // if the user selects a specific keyword, show image with the same keyword //
+      
+     
       if (ImageList.keyword === this.state.keyword)return true;
+
+      // if the user selects a specific horns, show image with the same horns //
+
+      if (ImageList.horns === this.state.horns)return true;
       
       return false;
     });
@@ -27,25 +52,24 @@ export default class App extends React.Component {
     <div className="App">
       <Header/>
       <div className="dropdown">
-          <p>Sort By Creature:</p>
-        <form className="form">
-  
-        <select value={this.state.keyword} onChange={(e)=> {this.setState({keyword: e.target.value})}} name="creatures" id="creatures">
-          
-          <option value="select">Select</option>
-          <option value="narwhal">Narwhal</option>
-          <option value="rhino">Rhino</option>
-          <option value="unicorn">Unicorn</option>
-          <option value="triceratops">Triceratops</option>
-          <option value="mouflon">Mouflon</option>
-          <option value="markhor">Markhors</option>
-          <option value="addax">Addax</option>
-          <option value="chameleon">Chameleon</option>
-          <option value="lizard">Lizard</option>
-          <option value="dragon">Dragon</option>
-        </select>
-        </form>
-      
+        <div className="dropdown-box">
+          Sort By Creature:
+          <Dropdown
+             
+              currentValue={this.state.keyword}
+              handleChange={this.keywordChange}
+              options={['narwhal', 'unicorn', 'rhino', 'unilego', 'triceratops', 'markhor', 'mouflon', 'addax', 'chameleon', 'lizard', 'dragon']}
+          />
+        </div>
+        <div className="dropdown-box">
+          Sort by # of Horns:
+          <Dropdown
+              
+              currentValue={this.state.horns}
+              handleChange={this.hornsChange}
+              options={['1', '2', '3', '100']}
+          />
+        </div>
       </div>
       <div className="image-gallery">
         <ImageList filteredAnimals= {filteredAnimals}/>
